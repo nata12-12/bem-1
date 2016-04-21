e102d33435d99ed160c2dbf1a5d14446ea6daf8e
nnn <nata_home12_12@mail.ru>
2016-04-21 18:10:10 +0400

    index

---
+++ desktop.bundles/index/1_index.bemjson.js
@@ -0,0 +1,42 @@
+({
+    block: 'page',
+    title: 'Hello, World!',
+    styles: [
+        { elem: 'css', url: 'index.min.css' },
+        '<!--[if IE]>',
+            { elem: 'css', url: 'index.min.ie.css' },
+        '<![endif]-->',
+        '<!--[if IE 8]>',
+            { elem: 'css', url: 'index.min.ie8.css' },
+        '<![endif]-->',
+        '<!--[if IE 9]>',
+            { elem: 'css', url: 'index.min.ie9.css' },
+        '<![endif]-->'
+    ],
+	head: [
+		{ elem: 'meta', attrs: { name: 'description', content: ''}},
+		{ elem: 'css', url: 'index.min.css'}
+	],
+    scripts: [
+        { elem: 'js', url: 'index.min.js' }
+    ],
+    content: [
+	{
+		block: 'head',
+		content: {
+			block: 'layout',
+			content: [
+				{
+					elem: 'left',
+					content: 'left here'
+				},
+				{
+					elem: 'right',
+					content: 'right here'
+				}
+			]
+		}
+	}
+    ]
+});
+
