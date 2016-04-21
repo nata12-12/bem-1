c1dc4c7c32de4af1a722ed1871cf2a6b550366d6
nnn <nata_home12_12@mail.ru>
2016-04-21 18:08:29 +0400

    1

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -13,10 +13,42 @@
             { elem: 'css', url: 'index.min.ie9.css' },
         '<![endif]-->'
     ],
+	head: [
+		{ elem: 'meta', attrs: { name: 'description', content: ''}},
+		{ elem: 'css', url: 'index.min.css'}
+	],
     scripts: [
         { elem: 'js', url: 'index.min.js' }
     ],
     content: [
-        'Hello, World!'
+	{
+		block: 'head',
+		content: {
+			block: 'layout',
+			content: [
+				{
+					elem: 'left',
+					content: 'left here'
+				},
+				{
+					elem: 'right',
+					content: {
+						block: 'logo',
+						content: [
+							{
+								block: 'image',
+								attrs: { src: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png'}
+							},
+							{
+								elem: 'slogan',
+								content: 'Nata'
+							}
+						]
+					}
+				}
+			]
+		}
+	}
     ]
 });
+
